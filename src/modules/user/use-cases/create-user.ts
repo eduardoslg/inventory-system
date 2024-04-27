3c8520f5f6de8f09cf103243ae4403577e23bbb2
eduardoslg <eduardo.seve@hotmail.com>
2024-04-27 16:09:06 -0300

    fix: melhorar validação ao criar usuário

--- src/modules/user/use-cases/create-user.ts
+++ src/modules/user/use-cases/create-user.ts
@@ -12,6 +12,14 @@ export class CreateUser {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   public async execute({ email, name, password }: CreateUserDTO) {
+    if (!email) {
+      throw new AppError('É necessário informar o email.')
+    }
+
+    if (!password) {
+      throw new AppError('É necessário informar a senha.')
+    }
+
     const emailExists = await this.usersRepository.findByEmail(email)
 
     if (emailExists) {
@@ -32,7 +40,6 @@ export class CreateUser {
       id: user.id,
       email: user.email,
       name: user.name,
-      created_at: user.created_at,
     }
   }
 }
